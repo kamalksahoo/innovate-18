ef8972c837ab1dc65bf447606d93789a5794ee36
kamal kant sahoo <kamal44399@gmail.com>
2022-02-11 11:50:59 +0530

    error page created

--- organic_frontend-master/src/screens/Error/Error.js
+++ organic_frontend-master/src/screens/Error/Error.js
@@ -1,8 +1,29 @@
-import React from 'react';
- const Error=()=> {
-  return <div>
-      <h1>page not found</h1>
-  </div>;
-}
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Error.css";
+const Error = () => {
+  return (
+    <div>
+      <div id="error-page">
+        <div class="content">
+          <h2 class="header" data-text="404">
+            404
+          </h2>
+          <h4 data-text="Opps! Page not found">
+            Opps! Page not found
+          </h4>
+          <p>
+            Sorry, the page you're looking for doesn't
+            exist. If you think something is broken, report
+            a problem.
+          </p>
+          <div class="btns">
+            <Link to="/">Go To Home</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Error;
